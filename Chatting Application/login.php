5753f89577e47c1c4f9803fd9f73e82d5acc60e5
venkataramgithub <venkataramulu9100@gmail.com>
2023-04-30 07:20:46 +0530

    screenshots added

--- Chatting Application/login.php
+++ Chatting Application/login.php
@@ -27,9 +27,54 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>RChat Application</title>
 	<link rel="stylesheet" href="./css/login.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<script>
+		function messagehide() {
+			dom=document.getElementById("message").style;
+			dom.visibility="hidden";
+		}
+		function showpassword() {
+			var eyeopen=document.getElementById("eyeopen").style;
+			var eyeclose=document.getElementById("eyeclose").style;
+			var password=document.getElementById("password");
+			if(password.type=="password"){
+				password.type="text";
+				eyeclose.display="none";
+				eyeopen.display="block";
+			}
+			else{
+				password.type="password";
+				eyeclose.display="block";
+				eyeopen.display="none";
+			}
+
+		}
+	</script>
 </head>
 <body>
+	<?php
+		if(isset($_POST['submit'])){
+		$email=mysqli_real_escape_string($conn,$_POST['email']);
+		$password=mysqli_real_escape_string($conn,$_POST['password']);
+
+		$select=mysqli_query($conn,"select * from registration where email='$email' and password='$password'");
+		if(mysqli_num_rows($select)>0){
+			$row=mysqli_query($conn,"update registration set status='Active' where email='$email'");
+			if($row){
+				$result=mysqli_fetch_assoc($select);
+				$_SESSION['userid']=$result['sl'];
+				header('location:chathome.php');
+			}
+			else{
+				echo '<div class="message" id="message">Status Not updated<span onclick="messagehide()">&times;</span></div>';
+			}
+			
+		}
+		else{
+			echo '<div class="message" id="message">Entered details are incorrect<span onclick="messagehide()">&times;</span></div>';
+		}
+	}
+	?>
 	<section class="container">
 		<div class="header">
 			<img src="./css/ramlogo.png" alt="logo">
@@ -40,15 +85,15 @@
 			<form method="post" action="" enctype="multipart/form-data">
 				<div class="form-group">
 					<label>Enter Email</label>
-					<input type="email" name="email" required>
+					<input type="email" name="email" placeholder="Enter Your Email" required>
 				</div>
 				<div class="form-group">
 					<label>Enter Password</label>
-					<input type="password" name="password" required>
-					<p><input type="checkbox" name="check">show password</p>
+					<input type="password" name="password" id="password" placeholder="Enter Your Password" required>
+					<span id="eyeopen" onclick="showpassword()"><i class="fas fa-eye"></i></span><span id="eyeclose" onclick="showpassword()"><i class="fas fa-eye-slash"></i></span>
 				</div>
 				<button name="submit">Login</button>
-				<span>Don't hava an account?<a href="signup.php">signup now</a></span>
+				<span style="margin-left:100px;">Don't hava an account?<a href="signup.php">signup now</a></span>
 			</form>
 		</div>
 	</section>
--- Chatting Application/signup.php
+++ Chatting Application/signup.php
@@ -1,29 +1,5 @@
 <?php
-	include_once('config.php');
-	if(isset($_POST['submit'])){
-		$fname=mysqli_real_escape_string($conn,$_POST['fname']);
-		$lname=mysqli_real_escape_string($conn,$_POST['lname']);
-		$gender=mysqli_real_escape_string($conn,$_POST['gender']);
-		$email=mysqli_real_escape_string($conn,$_POST['email']);
-		$password=mysqli_real_escape_string($conn,$_POST['password']);
-		$phone=mysqli_real_escape_string($conn,$_POST['phone']);
-		
-		$image=$_FILES['image']['name'];
-		$image_temp_name=$_FILES['image']['tmp_name'];
-
-		$select=mysqli_query($conn,"select * from registration where email='$email' and password='$password'");
-		if(mysqli_num_rows($select)>0){
-			echo 'user already exist';
-		}
-		else{
-			$sql=mysqli_query($conn,"insert into registration(fname,lname,gender,email,password,phone,image) values('$fname','$lname','$gender','$email','$password','$phone','$image')");
-			if($sql){
-				move_uploaded_file($image_temp_name,'uploads/'.$image);
-				echo 'data uploaded successfully';
-				header('location:login.php');
-			}
-		}
-	}	
+	include_once('config.php');	
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,8 +8,40 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>RChat Application</title>
 	<link rel="stylesheet" href="./css/signup.css">
+	<script type="text/javascript">
+		function messagehide() {
+			dom=document.getElementById("message").style;
+			dom.visibility="hidden";
+		}
+	</script>
 </head>
 <body>
+	<?php
+		if(isset($_POST['submit'])){
+			$fname=mysqli_real_escape_string($conn,$_POST['fname']);
+			$lname=mysqli_real_escape_string($conn,$_POST['lname']);
+			$gender=mysqli_real_escape_string($conn,$_POST['gender']);
+			$email=mysqli_real_escape_string($conn,$_POST['email']);
+			$password=mysqli_real_escape_string($conn,$_POST['password']);
+			$phone=mysqli_real_escape_string($conn,$_POST['phone']);
+			
+			$image=$_FILES['image']['name'];
+			$image_temp_name=$_FILES['image']['tmp_name'];
+
+			$select=mysqli_query($conn,"select * from registration where email='$email' and password='$password'");
+			if(mysqli_num_rows($select)>0){
+				echo '<div class="message" id="message">User already exist<span onclick="messagehide()">&times;</span></div>';
+			}
+			else{
+				$sql=mysqli_query($conn,"insert into registration(fname,lname,gender,email,password,phone,image) values('$fname','$lname','$gender','$email','$password','$phone','$image')");
+				if($sql){
+					move_uploaded_file($image_temp_name,'uploads/'.$image);
+					echo '<div class="message" id="message">Data uploaded successfully<span onclick="messagehide()">&times;</span></div>';
+					header('location:login.php');
+				}
+			}
+		}
+	?>
 	<section class="container">
 		<div class="header">
 			<img src="./css/ramlogo.png" alt="logo">
@@ -42,13 +50,15 @@
 		<div class="register">
 			<p>RChat SignUp</p>
 			<form method="post" action="" enctype="multipart/form-data">
-				<div class="form-group">
-					<label>First Name</label>
-					<input type="text" name="fname" required>
-				</div>
-				<div class="form-group">
-					<label>Last Name</label>
-					<input type="text" name="lname" required>
+				<div class="name-group">
+					<div class="form-group">
+						<label>First Name</label>
+						<input type="text" name="fname" placeholder="Enter FirstName" required>
+					</div>
+					<div class="form-group">
+						<label>Last Name</label>
+						<input type="text" name="lname" placeholder="Enter LastName" required>
+					</div>
 				</div>
 				<div class="form-group">
 					<label>Gender</label>
@@ -61,22 +71,22 @@
 				</div>
 				<div class="form-group">
 					<label>Enter Email</label>
-					<input type="email" name="email" required>
+					<input type="email" name="email" placeholder="Enter Your Email" required>
 				</div>
 				<div class="form-group">
 					<label>Enter Password</label>
-					<input type="password" name="password" required>
+					<input type="password" name="password" placeholder="Enter Your Password" required>
 				</div>
 				<div class="form-group">
 					<label>Enter Phone</label>
-					<input type="text" name="phone" required>
+					<input type="text" name="phone" placeholder="Enter Your Phone" required>
 				</div>
 				<div class="form-group">
 					<label>Select Image</label>
 					<input type="file" name="image" required>
 				</div>
 				<button name="submit">SignUp</button>
-				<span>Already signed up?<a href="login.php">login now</a></span>
+				<span style="margin-left:150px;">Already signed up?<a href="login.php">login now</a></span>
 			</form>
 		</div>
 	</section>
